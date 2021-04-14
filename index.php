f69cd3d8455997d912dea57c0546d36d82afbf32
Romario Alves <Romariogn10@gmail.com>
2021-04-14 13:43:23 -0300

    title main e nav-bar main

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     </head>
     <body>
         <div class="container-fluid p-0">
-            <nav class="navbar navbar-expand-lg navbar-light justify-content-between align-items-center">
+            <nav class="navbar navbar-expand-md navbar-light justify-content-between align-items-center">
                 <a href="" class="navbar-brand"><img class="img-logo" src="assets/images/logomazukim.png" alt="Mazukim"></a>
                 <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
                     <span class="navbar-toggler-icon"></span>
@@ -45,20 +45,50 @@
                         <div>
                             <h3 class="title">CADASTRE-SE PARA RECEBER CONTEÚDO EM PRIMEIRA MÃO!</h3>   
                         </div>
-                        <form class="form col-sm-8 my-2 my-lg-0">
+                        <form class="form col my-2 my-lg-0">
                             <div class="col d-flex">
-                                <input id="email" class="form-control w-100 mr-2 " type="search" placeholder="Seu E-mail*" aria-label="Search" required>
+                                <input id="email" class="form-control mr-2 " type="search" placeholder="Seu E-mail*" aria-label="email" required>
                                 <button class="btn btn-email my-2 my-sm-0" type="submit">Enviar</button>
                             </div>     
                         </form>
                     </div>
                 </div>
                 <div class="col-sm-4">
-                    <div class="banner">
-                        
+                    <div class="banner">    
                     </div>
                 </div>
             </div>
+            <div class="container d-flex flex-column align-items-center">
+                <div class="title-main">
+                    <h1 class="text-secondary"> Muito Conteúdo de Marketing Digital.<strong> Vamos Performar!</strong></h1>
+                </div>
+                <nav class="navbar navbar-expand-md navbar-light justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        <div class="navbar-collapse" >
+                            <div class="navbar-nav ">
+                                <a href="#" id="me-nav" class="nav-item nav-link active rounded h5 mr-1 ml-1 ">Dicas de Marketing</a>
+                                <a href="#" id="me-nav" class="nav-item nav-link active rounded h5 mr-1 ml-1 ">Facebook</a>
+                                <a href="#" id="me-nav" class="nav-item nav-link active rounded h5 mr-1 ml-1 ">Googles Ads</a>
+                                <a href="#" id="me-nav" class="nav-item nav-link active rounded h5 mr-1 ml-1 ">Instagram</a>
+                                <a href="#" id="me-nav" class="nav-item nav-link active rounded h5 mr-1 ml-1 ">Notícias</a>
+                                <a href="#" id="me-nav" class="nav-item nav-link active rounded h5 mr-1 ml-1 ">SEO</a>
+                            </div>
+                        </div>
+                    </div>                
+                </nav>
+
+
+            </div>
+            <div class="container d-flex">
+                <div class="col-sm-8">
+                    Lado Left
+                </div>
+                <div class="col-sm-4">
+                    Lado right
+                </div>
+            </div>
+
+
         </div>
 
     <script type="text/javascript" src="assets/js/jquery-3.6.0.min.js"></script>
